e10ec705298ef20f40c1c73106eeefec665a8b8f
ChaatruSrikar <chaatrusrikar@gmail.com>
2019-11-06 13:12:51 +0530

    Book id done

--- Git/src/com/training/Application.java
+++ Git/src/com/training/Application.java
@@ -5,9 +5,8 @@ import java.util.*;
 public class Application {
 
 	public static void main(String[] args) {
-		List<? extends Object> nameList = Arrays.asList("Ramesh",101,"Bangalore");
 		
-
-	}
-
+		Book book = new Book(101,"GOT");
+		System.out.println(book.getBookId());
+}
 }
