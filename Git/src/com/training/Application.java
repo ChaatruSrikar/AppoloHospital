1379b12df84ea88b043cefc93b79c4081752b03b
ChaatruSrikar <chaatrusrikar@gmail.com>
2019-11-06 12:49:39 +0530

    My first Commit

---
+++ Git/src/com/training/Application.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import java.util.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+		List<? extends Object> nameList = Arrays.asList("Ramesh",101,"Bangalore");
+		
+
+	}
+
+}
